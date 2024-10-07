bf70533d36921d61e67e687d246169fd67a00e09
Natpakanun1112 <natpakanumkhosuwan@gmail.com>
2024-10-07 16:54:09 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+first_name= input("Hey, whats' your first name? : ")
+last_name = input("And your last name? : ")
+
+print (first_name)
+print (last_name)
+print ("Well, pleased to meet you, " + first_name + " " + last_name + "." )
+ 
