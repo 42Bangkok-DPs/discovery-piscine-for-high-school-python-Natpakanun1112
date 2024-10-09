9adf8d554b26677c7631ea1e9a2e72cae759d5a6
Natpakanun1112 <natpakanumkhosuwan@gmail.com>
2024-10-09 10:52:15 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env.python3
+n1 = float(input("Give me the first number: "))
+n2 = float(input("Give mr the second number: "))
+print("Thank you!")
+print(f"{n1} + {n2} = " , n1+n2)
+print(f"{n1} - {n2} = " , n1-n2)
+print(f"{n1} / {n2} = " , n1/n2)
+print(f"{n1} * {n2} = " , n1*n2)
